d9248d8164338324331f8f0e698533fa94c4dbe8
Greta Simonyan <gsimonyan228@gmail.com>
2019-12-04 16:17:08 +0400

    lil commit

--- src/components/SWorkSlider/index.js
+++ src/components/SWorkSlider/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import './style.css';
-import joshua from '../../assets/images/joshua.png';
-
 import styled from "styled-components";
-
-
-const Slider_back = styled.div`
-    width: 100%;
-    height: 240px;
-    margin: 100px 0;
-`;
-
-const Slider = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 0 auto;
-    max-width: 1200px;
-`;
+import View from '../../styled/View';
+import Text from '../../styled/Text';
+import joshua from '../../assets/images/joshua.png';
 
 const Button = styled.button`
     border: none;
@@ -29,33 +15,26 @@ const Button = styled.button`
     color: ${props => (props.left ? "blue" : "red" )};
 `;
 
-const Slider_intro = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0 30px;
-`;
-
 class SWorkSlider extends React.Component{
     render(){
         return(
-            <Slider_back>
-                <Slider>
-
+            <View w='100%' h='240px' m='100px 0'>
+                <View flex justC='center' m='0 auto' maxW='1200px'>
                     <Button left>&#60;</Button>
 
-                    <Slider_intro>
+                    <View flex justC='space-between' alignI='center' m='0 30px'>
                         <img src={joshua}/>
-                        <div className='citate-autor'>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."</p>
-                            <div>-Joshua Earle</div>
-                        </div>
-                    </Slider_intro>
+                        <View p='40px'>
+                            <Text fontSize='22px' fontF="'Arial,Helvetica'" fontStyle='italic' op='0.5'>
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
+                            </Text>
+                            <View fontSize='25px' fontF="'Kaushan Script'">-Joshua Earle</View>
+                        </View>
+                    </View>
 
                     <Button right>&#62;</Button>
-                    
-                </Slider>
-            </Slider_back>
+                </View>
+            </View>
         )
     }
 }
--- src/components/SomeWork/index.js
+++ src/components/SomeWork/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './style.css';
+import styled from "styled-components";
+import View from '../../styled/View';
+
 import work1 from '../../assets/images/work-1.jpg';
 import work2 from '../../assets/images/work-2.jpg';
 import work3 from '../../assets/images/work-3.jpg';
@@ -7,19 +10,18 @@ import work4 from '../../assets/images/work-4.jpg';
 import work5 from '../../assets/images/work-5.jpg';
 import work6 from '../../assets/images/work-6.jpg';
 import work7 from '../../assets/images/work-7.jpg';
-
 import Section from '../../Section';
 
 function SomeWork(){
     return(
-        <div className='SomeWork'>
+        <View mT='100px'>
             <Section 
                 suptitle='What we do' 
                 title='Some of our work' 
                 text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
             />
 
-            <div className='pics'>
+            <View flex h='780px' fF='wrap' fD='column'>
                 <div><img src={work1}/></div>
                 <div><img src={work3}/></div>
                 <div><img src={work2}/></div>
@@ -27,8 +29,8 @@ function SomeWork(){
                 <div className='long'><img src={work5}/></div>
                 <div><img src={work6}/></div>
                 <div><img src={work7}/></div>
-            </div>
-        </div>
+            </View>
+        </View>
     )
 }
 
